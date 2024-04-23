8398a68463651ae41efa8035368a4d7db9d62da4
jolan <jolan.philippe@gmail.com>
2024-04-23 09:24:56 +0200

    Revert Farid's change

--- model-maker/concerto.py
+++ model-maker/concerto.py
@@ -538,7 +538,7 @@ class Connect(Instruction):
                + hash(self._using_port) + hash(self._user)
 
 
-class disconnect(Instruction):
+class Disconnect(Instruction):
 
     def __init__(self, provider: str, providing_port: str, user: str, using_port: str):
         self._provider = provider
@@ -568,7 +568,7 @@ class disconnect(Instruction):
         return f"disconnect({self._provider}, {self._providing_port}, {self._user}, {self._using_port})"
 
     def __eq__(self, other):
-        if isinstance(other, disconnect):
+        if isinstance(other, Disconnect):
             return other.provider() == self.provider() and other.providing_port() == self.providing_port() \
                    and other.user() == self.user() and other.using_port() == self.using_port()
         return False
